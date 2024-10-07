27ad1d6c6625d9164d796d5b7dd552dcd44f1e2d
oliverzimmermanstu <oliver.zimmerman.stu@cedarpark.org>
2024-10-07 14:08:31 -0700

    Step 1 completed

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+print("Joe has a dog named Spike who loves to play kickball.")
+print("in kickball, you kick a  blue ball with your foot 3 times")
+print("Joe has a nice wife named Joanne. She loves to bake, in fact, she owns a world renowned bakery in Dublin named Joanne's Bakery.")
+print("Last week, the two of them went on a hike up Mount everest. They were very tired when they got back")
+print("Joe and joanne finally saved up enough money to go on a vacation. They are going to Seattle.")
+
+
+
