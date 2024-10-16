86da95fb5297008a424695063845fb4a719bf983
oliverzimmermanstu <oliver.zimmerman.stu@cedarpark.org>
2024-10-16 14:40:56 -0700

    Step 3

--- madlibs.py
+++ madlibs.py
@@ -1,8 +1,29 @@
-print("Joe has a dog named Spike who loves to play kickball.")
-print("in kickball, you kick a  blue ball with your foot 3 times")
-print("Joe has a nice wife named Joanne. She loves to bake, in fact, she owns a world renowned bakery in Dublin named Joanne's Bakery.")
-print("Last week, the two of them went on a hike up Mount everest. They were very tired when they got back")
-print("Joe and joanne finally saved up enough money to go on a vacation. They are going to Seattle.")
+name = input("Enter a name: ")
+pet = input("Enter a pet: ")
+animal = input("Enter a animal: ")
+adj1 = input("Enter a adjective: ")
+adj2 = input("Enter a adjective: ")
+adj3 = input("Enter a adjective: ")
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter a verb: ")
+verb3 = input("Enter a verb: ")
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter a noun: ")
+noun3 = input("Enter a noun: ")
+noun4 = input("Enter a noun: ")
+num1 = input("Enter a number: ")
+num2 = input("Enter a number: ")
+num3 = input("Enter a number: ")
+
+
+
+
+
+print(name + " has a " + animal + " named Spike who loves to " + verb1 + " " + noun1)
+print(name + " also likes to " + verb2 + ". They do this " + num1 + " times a day")
+print(name + "s best friend is a guy named " + noun2 + ". They like to " + verb3 + " very " + adj1 + ".")
+print("Last week, the two of them went on a " + adj2 + " " + noun2 + " up Mount " + noun3 + ". It was " + num2 + " miles long." + " They were very " + adj3 + " when they got back")
+print(pet + " is Joe's pet." + noun4 + "is where Joe is from. " + "And lastly, Joe is " + num3 + " years old.")
 
 
 
